55a417ac7018e298578b8a75e8b964e1ebe243a5
Ted Sazon <tedsazon@gmail.com>
2018-12-17 19:15:21 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  if block_given?
+    x = 0
+
+    while x < array.length
+      yield array[x]
+      x = x + 1
+    end
+  end
+  array
 end
